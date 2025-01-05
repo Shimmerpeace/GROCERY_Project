e0692c74bba221f46e9a99346a0e3836a9eef71a
Shimmerpeace <144375792+Shimmerpeace@users.noreply.github.com>
2025-01-05 22:31:46 +0100

    Update app.js

--- Project/app.js
+++ Project/app.js
@@ -1,5 +1,7 @@
 
 //****** SELECT ITEMS ********/
+// querySelector() selects or returns the first element in the HTML documents that matches
+//  the specified selector (e.g .alert)
 const alert = document.querySelector(".alert");
 const form = document.querySelector(".grocery-form");
 const grocery = document.getElementById("grocery");
@@ -9,70 +11,128 @@ const list = document.querySelector(".grocery-list");
 const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
-let getElement;
+let editElement;
 let editFlag = false;
 let editID = "";
 
-// *** EVENT LISTENERS *****
-// submit form
-form.addEventListener("submit", addItem);
-
-// **** FUNCTIONS *****
-function addItem(e) {
+// *** EVENT LISTENERS FOR FORMS*****
+form.addEventListener("submit", addItem); // submit form
+// **** FUNCTIONS FOR FORMS *****
+function addItem(e) { 
     e.preventDefault();
     const value = grocery.value;
-    const id = new Date().getTime().toString();
+    const id = new Date().getTime().toString();        
+
+    /* 
+    if(value !== "" && editFlag === false) {console.log("add item to the list");}
+    else if(value !== "" && editFlag === true) {console.log("editing");}
+    else {console.log("empty value");}
+    */
+
     if (value && !editFlag) {
-        const element = document.createElement("article")
-// add class
-element.classList.add("grocery-item");
-// id
-const attr = document.createAttribute("data-id");
-attr.value = id;
-element.setAttributeNode(attr);
-element.innerHTML = ` <p class="title">${value}</p>
+        const element = document.createElement("article");       
+        element.classList.add("grocery-item"); // add class
+        const attr = document.createAttribute("data-id"); // id
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = `<p class="title">${value}</p>
         <div class="btn-container">
-          <button type="button" class="edit-btn">
-            <i class="fas fa-edit"></i>
-          </button> 
-          <button type="button" class="delete-btn">
-        <i class="fas fa-trash"></i>
-    </button>                    
-</div> ` 
-// append child
-list.appendChild(element);
-// display alert
-displayAlert("item added to the list", "success"); 
-// show container
-container.classList.add("show-container");
-// add to local storage
-addToLocalStorage(id,value);
-// set back to default
-setBackToDefault()
-    } else if (value && editFlag) {
-        console.log("editing"); 
+        <button type="button" class="edit-btn"> <i class="fas fa-edit"></i> </button>
+        <button type="button" class="delete-btn"> <i class="fas fa-trash"></i> </button>
+        </div>`;
+        const deleteBtn = element.querySelector(".delete-btn");
+        const editBtn = element.querySelector(".edit-btn");
+
+        deleteBtn.addEventListener("click", deleteItem);
+        editBtn.addEventListener("click", editItem);
+
+        list.appendChild(element); // append child
+        displayAlert("item added to the list", "success"); // display alert      
+        container.classList.add("show-container"); // show container
+        addToLocalStorage(id,value); // add to local storage
+        setBackToDefault(); // set back to default
+    }
+     else if (value && editFlag) {
+        editElement.innerHTML = value;
+        displayAlert("value changed", "success"); 
+        // edit local storage
+        editLocalStorage(editID, value);
+        setBackToDefault()
     } else {
        displayAlert("please enter value", "danger"); 
     }
 }
-
-// **** display alert *****
-function displayAlert(text,action){
-    alert.textContent = text;
-    alert.classList.add('alert-${action}');
+// delete function
+function deleteItem(e) {
+const element = e.currentTarget.parentElement.parentElement;
+const id = element.dataset.id;
+list.removeChild(element);
+if (list.children.length === 0) {
+    container.classList.remove("show-container");
+}
+displayAlert("item removed", "danger");
+setBackToDefault();
+// remove from local storage
+// removeFromLocalStorage(id);
 }
 
-// **** remove alert *****
-setTimeout(function(){
+// edit function
+function editItem() {
+    const element = e.currentTarget.parentElement.parentElement;
+    // set edit item
+    editElement = e.currentTarget.parentElement.previousElementSibling;
+    // set form value
+    grocery.value = editElement.innerHTML;
+    editFlag = true;
+    editID = element.dataset.id;
+    submitBtn.textContent = "edit";    
+}
+
+//  display alert function
+function displayAlert(text,action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    // remove alert 
+    setTimeout(function() {
     alert.textContent = "";
-    alert.classList.remove('alert-${action}');
-},1000)
+    alert.classList.remove(`alert-${action}`);
+},1000);
+}
+
+// *** EVENT LISTENERS TO CLEAR ITEMS *****
+clearBtn.addEventListener("click", clearItems); // clear items
+
+
+
+// **** FUNCTIONS FOR CLEAR ITEMS *****
+function clearItems() {
+    const items = document.querySelectorAll(".grocery-item");
+
+    if (items.length > 0) {
+        items.forEach(function (item) {
+            list.removeChild(item);
+        });
+    }
+    container.classList.remove("show-container");
+    displayAlert("empty list", "danger");
+    setBackToDefault();
+    //localStorage.removeItem("list");
+}
 
 // set back to default
 function setBackToDefault() {
-    console.log("set back to default");
+    grocery.value = "";
+    editFlag = false;
+    editID = "";
+    submitBtn.textContent = "submit";
 }
-//****LOCAL STORAGE  */
+//****LOCAL STORAGE  ************/
 function addToLocalStorage(id, value) {
-    console.log("added to local storage");
+    // console.log("added to local storage");
 }
+function removeFromLocalStorage(id) {}
+function editLocalStorage(id, value) {
+
+}
+//**** SETUP ITEMS  ************/
